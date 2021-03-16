39e619a35e8b48ba4092e9957aa880cd4b52de12
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-03-16 13:07:34 -0300

    termina o programa AreaTriangulo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desenvolver um programa que calcule a área de um triangulo.
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Calcule a área de um triângulo.");
+            Console.ResetColor();
+            Console.WriteLine();
+            double area, baseB,alturaH = 0;
+            
+            Console.WriteLine("Informe a base do triângulo: ");
+            Console.WriteLine();
+
+            baseB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a altura do triângulo: ");
+            Console.WriteLine();
+            alturaH = double.Parse(Console.ReadLine());
+
+            area = (baseB * alturaH) /2 ;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Area do Triângulo é : =  {area}");
+            Console.ResetColor();
+
+        }
+    }
+}
